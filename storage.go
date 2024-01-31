f08dffc34d4d5eb0a703af033bb61d2396154b15
richard.li <lirichen32@gmail.com>
2024-01-31 19:48:28 +0800

    Update for log

--- storage.go
+++ storage.go
@@ -2,10 +2,10 @@ package record
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
+	"go.uber.org/zap"
 )
 
 type StorageConfig struct {
@@ -17,16 +17,12 @@ type StorageConfig struct {
 }
 
 func (r *Recorder) UploadFile(filePath string, fileName string) {
-	fmt.Println(fileName)
 	ctx := context.Background()
-	fmt.Println(r.Storage.Endpoint)
-
 	endpoint := r.Storage.Endpoint
 	accessKeyID := r.Storage.AccessKey
 	secretAccessKey := r.Storage.SecretKey
 	bucketName := r.Storage.Bucket
 	useSSL := r.Storage.UseSSL
-	fmt.Println(r.Storage.AccessKey)
 	// Initialize minio client object.
 	minioClient, err := minio.New(endpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
@@ -34,7 +30,7 @@ func (r *Recorder) UploadFile(filePath string, fileName string) {
 	})
 
 	if err != nil {
-		fmt.Println(err.Error())
+		r.Error("create minioClient error:", zap.Error(err))
 	}
 
 	// Make a new bucket called testbucket.
@@ -45,26 +41,27 @@ func (r *Recorder) UploadFile(filePath string, fileName string) {
 		// Check to see if we already own this bucket (which happens if you run this twice)
 		exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
 		if errBucketExists == nil && exists {
-			fmt.Println("We already own %s\n", bucketName)
+
+			r.Info("Bucket already Exists", zap.String("bucket", bucketName))
 		} else {
-			fmt.Println(err.Error())
+			r.Error("Create Bucket Error:", zap.Error(err))
 		}
 	} else {
-		fmt.Println("Successfully created %s\n", bucketName)
+		r.Info("Successfully created Bucket:", zap.String("bucket", bucketName))
 	}
 
 	// Upload the test file
 	// Change the value of filePath if the file is in another location
 	objectName := fileName
 	fileFullPath := filePath + "/" + objectName
-	fmt.Println("Prepare Upload  Path: %s  fileName: %s", fileFullPath, objectName)
+	r.Info("Prepare Upload  Path:  fileName:", zap.String("filePath", filePath), zap.String("objectName", objectName))
 	contentType := "application/octet-stream"
 
 	// Upload the test file with FPutObject
 	info, err := minioClient.FPutObject(ctx, bucketName, objectName, fileFullPath, minio.PutObjectOptions{ContentType: contentType})
 	if err != nil {
-		fmt.Println(err.Error())
+		r.Error("Minio PutObject Error:", zap.Error(err))
 	}
 
-	fmt.Println("Successfully uploaded %s of size %d\n", objectName, info.Size)
+	r.Info("Successfully uploaded of size ", zap.String("objectName", objectName), zap.Int64("Size", info.Size))
 }
